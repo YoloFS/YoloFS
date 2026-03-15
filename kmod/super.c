371fd6a7b2eb32b19be46b9877a45eb5b086d835
ShawnZhong <github@shawnzhong.com>
2026-03-15 17:03:42 -0500

    Bug fix

--- kmod/super.c
+++ kmod/super.c
@@ -324,6 +324,11 @@ static int agfs_init_fs_context(struct fs_context *fc)
 
 static void agfs_kill_super(struct super_block *sb)
 {
+	/* Pre-prune cached dentries (especially those created by chroot
+	 * lookups through the overlay of "/") so that the aggressive
+	 * shrink_dcache_for_umount() inside kill_anon_super() does not
+	 * find busy dentries and emit BUG warnings. */
+	shrink_dcache_sb(sb);
 	kill_anon_super(sb);
 }
 
